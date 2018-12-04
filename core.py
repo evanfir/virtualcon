2cedb7031609d66133e7a7b572df68cab1e4c792
Evan Firoozi <evanfir@gmail.com>
2018-12-04 00:44:34 -0800

    test the API
    core calles the API and get requests

--- core.py
+++ core.py
@@ -1,5 +1,20 @@
-from dbhandler import dbHandler
+# from dbhandler import dbHandler
+from myFlask import runFlask
 
-myDB = dbHandler("student", "student")
-keywords = ["test1", "test2"]
-# myDB.insertQuestion("question1", "answer1", keywords)
+runFlask()
+# myDB = dbHandler("qa", "qa")
+# studentDB = dbHandler("student", "student")
+# keywords = ["test1", "test2"]
+# # myDB.insertQuestion("question5", "answer5")
+# # myDB.insertQuestion("question6", "answer6")
+# # myDB.insertQuestion("question7", "answer7")
+# # myDB.insertQuestion("question8", "answer8")
+# # studentDB.insertStudentInfo(30, "first1", "last1", "CS")
+# # studentDB.insertStudentInfo(40, "first2", "last2", "Math")
+# # answer = myDB.retrieveAnswer("question1")
+# student = studentDB.retrieveStudentInfo(30)
+# # print()
+# # print(answer)
+# print(student)
+# # myDB.updateQuestion("question8", "nhhhhhr")
+# studentDB.updateStudentInfo(10, "hello", "bye", "lllll")
