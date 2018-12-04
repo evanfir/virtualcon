14b6cd6d2eefd8dd20ac40ffcf6cf5ce6e2f3190
Evan Firoozi <evanfir@gmail.com>
2018-12-04 00:42:44 -0800

    restful flask API for database
    3 classes:
    to call: /<classname>?key0=arg0&key1=arg1
    
    StudentInfo: student handles everything about student includeing id, name, lname, major
    methods:
    get -> return student info based on id
    post -> add to db
    put -> update db
    
    StudentApi:  can only retrieve a question's answer
    methods:
    get-> returns answer
    
    AdminApi: allows admin to add, edit or retrieve a question and answer
    methods:
    get -> returns answer
    post -> add question and answer
    put -> update question and answer

---
+++ myFlask.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from dbhandler import *
+
+
+app = Flask(__name__)
+api = Api(app)
+
+studentDB = dbHandler("student", "student") #initiate studentDB
+QuestionDB = dbHandler("qa", "qa") #initiate Question and Answers DB
+
+parser = reqparse.RequestParser() 
+
+#student handles everything about student includeing id, name, lname, major
+#to call: /StudentInfo?id=000&first=XXXX&last=XXXX&major=XXXX
+class StudentInfo(Resource):
+    parser.add_argument('id')
+    parser.add_argument('first')
+    parser.add_argument('last')
+    parser.add_argument('major')
+
+    def get(self):
+        args = parser.parse_args()
+
+        studentID = args["id"]
+        # print("\n\n\nstudentID: " + str(studentID) + "\n\n")
+
+        info = studentDB.retrieveStudentInfo(int(studentID))
+        if len(info) > 0:
+            return info, 200
+        else:
+            return "Student Not Found", 404
+
+    def post(self):
+        args = parser.parse_args()
+        studentDB.insertStudentInfo(int(args['id']), args['first'], args['last'], args['major'])
+        return args['id'], args['first'], args['last'], args['major'], 201
+        
+
+    def put(self):
+        args = parser.parse_args()
+        studentDB.updateStudentInfo(int(args['id']), args['first'], args['last'], args['major'])
+        return args['id'], args['first'], args['last'], args['major'], 201
+
+## student API can only retrieve a question's answer
+# to call: /StudentApi?question=XXXX XXXX
+class StudentApi(Resource):
+    parser.add_argument('question')
+    def get(self):
+        args = parser.parse_args()
+
+        question = args["question"]
+        answer = QuestionDB.retrieveAnswer(question)
+        if len(answer) > 0:
+            return answer, 200
+        else:
+            return "Question not found", 404
+
+## Admin API allows admin to add, edit or retrieve a question and answer
+# to call: /AdminApi?question=XXXXX&answer=XXXXX
+class AdminApi(Resource):
+    parser.add_argument('question')
+    parser.add_argument('answer')
+    def get(self):
+        args = parser.parse_args()
+
+        question = args["question"]
+        answer = QuestionDB.retrieveAnswer(question)
+        if len(answer) > 0:
+            return answer, 200
+        else:
+            return "Question not found", 404
+
+    def post(self):
+        args = parser.parse_args()
+
+        QuestionDB.insertQuestion(args['question'], args['answer'])
+        return ("Question: ", args['question'], "Answer: ", args['answer']), 201
+
+
+    def put(self):
+        args = parser.parse_args()
+
+        QuestionDB.updateQuestion(args['question'], args['answer'])
+        return ("Question: ", args['question'], "Answer: ", args['answer']), 201
+
+
+api.add_resource(StudentInfo, "/StudentInfo")
+api.add_resource(StudentApi, "/StudentApi")
+api.add_resource(AdminApi, "/AdminApi")
+
+def runFlask():
+    app.run(debug=True)
