3c70778cfae883e8503d3cb1bf5de171efb08caf
Evan Firoozi <evanfir@gmail.com>
2018-12-05 07:19:01 -0800

    delete form db, and its interface
    added delete functionality to the database for question and answer database and added its interface to the flask

--- myFlask.py
+++ myFlask.py
@@ -10,7 +10,7 @@ api = Api(app)
 
 studentDB = dbHandler("student", "student") #initiate studentDB
 QuestionDB = dbHandler("qa", "qa") #initiate Question and Answers DB
-SurveyDB = dbHandler("survey", "survey")
+SurveyDB = dbHandler("survey", "survey") #initiate Survey database
 
 parser = reqparse.RequestParser() 
 
@@ -35,13 +35,15 @@ class StudentInfo(Resource):
         else:
             return "Student Not Found", 404
 
-
+    ## insert a student info into the db
+    # @return: all the student info that from input and 201 for success
     def post(self):
         args = parser.parse_args()
         studentDB.insertStudentInfo(int(args['id']), args['first'], args['last'], args['major'])
         return args['id'], args['first'], args['last'], args['major'], 201
         
-
+    ## update a student info
+    # @return: all the student info that from input and 201 for success
     def put(self):
         args = parser.parse_args()
         studentDB.updateStudentInfo(int(args['id']), args['first'], args['last'], args['major'])
@@ -56,6 +58,7 @@ class StudentApi(Resource):
         args = parser.parse_args()
         question = args["question"]
         answer = QuestionDB.retrieveAnswer(question)
+        #check if any of the fields are NoneType
         if len(answer) > 0:
             return answer, 200
         else:
@@ -67,9 +70,14 @@ class StudentApi(Resource):
 class AdminApi(StudentApi):
     parser.add_argument('question')
     parser.add_argument('answer')
+
+    ## get request
+    # @return: answer to the question or error if question not found
     def get(self):
         args = parser.parse_args()
-        question = args["question"]
+        question = args['question']
+        answer = args['answer']
+        #check if any of the fields are NoneType
         if question is None:
             answer = QuestionDB.retrieveAnswer("*")
         else:
@@ -79,26 +87,44 @@ class AdminApi(StudentApi):
         else:
             return "Question not found", 404
 
-
+    # Insert into the db
+    # @return: question and answer that are added to the db
     def post(self):
         args = parser.parse_args()
         question = args['question']
         answer = args['answer']
+        #check if any of the fields are NoneType
         if question is not None and answer is not None:
             QuestionDB.insertQuestion(args['question'], args['answer'])
             return ("Question: ", args['question'], "Answer: ", args['answer']), 201
         else:
             return "Question or Answer or both are missing", 404
 
+    ## update entry
+    # @return: question and answer that has been changed or 404 if either is missing
     def put(self):
         args = parser.parse_args()
         question = args['question']
         answer = args['answer']
+        #check if any of the fields are NoneType
         if question is not None and answer is not None:
             QuestionDB.updateQuestion(question, answer)
             return ("Question: ", question, "Answer: ", answer), 201
         else:
             return "Question or Answer or both are missing", 404
+    
+    ## delete a question and answer
+    # @return: 202 for success, 404 for failure
+    def delete(self):
+        args = parser.parse_args()
+        question = args['question']
+        if question is not None:
+            QuestionDB.deleteQuestion(question)
+            return 202
+        else:
+            return "Question not found", 404
+
+
 
 
 ## Email client API handles emails student wanna send to the admin
@@ -109,12 +135,15 @@ class EmailApi(Resource):
     parser.add_argument('subject')
     parser.add_argument('body')
     
+    ## send an email 
+    # @return 201 for success
     def post(self):
         emailClient = EmailClient()
         args = parser.parse_args()
         sender = args['sender']
         subject = args['subject'] 
         body = args['body']
+        #check if any of the fields are NoneType
         if sender is None:
             sender = "N/A"
         if subject is None:
@@ -132,18 +161,25 @@ class EmailApi(Resource):
 class CrawlerApi(Resource):
     parser.add_argument("to")
     
+    ## get the link to the iframe from assist.org
+    # @return: a link
     def get(self):
         args = parser.parse_args()
         crawler = VirtualCrawler(args['to'])
         link = crawler.getiFrameLink()
         return link, 200
 
-
+## SurveyApi is the VSurvey class
+# to call: /SurveyApi?rate=5&comment=XXXX&id=000
+# get returns all the db
+# post insert a new row to the db
 class SurveyApi(Resource):
-    parser.add_argument("question")
+    parser.add_argument("rate")
     parser.add_argument("comment")
     parser.add_argument("id")
     
+    ## get all the survey results
+    # @return: json
     def get(self):
         results = SurveyDB.retrieveSurveyResults()
         # print("\n\n")
@@ -151,11 +187,14 @@ class SurveyApi(Resource):
         # print("\n\n")
         return results, 200
 
+    ## store a new survey result in db
+    # @return: entry and 201 for success
     def post(self):
         args = parser.parse_args()
         rate = args['rate']
         comment = args['comment']
         studentID = args['id']
+        #check if any of the fields are NoneType
         if rate is None:
             rate = -1
         if comment is None:
@@ -164,7 +203,8 @@ class SurveyApi(Resource):
             studentID = 0
         SurveyDB.insertSurvey(rate, comment, studentID)
         return rate, comment, studentID, 201
-        
+
+##handle arguments        
 api.add_resource(StudentInfo, "/StudentInfo")
 api.add_resource(StudentApi, "/StudentApi")
 api.add_resource(AdminApi, "/AdminApi")
@@ -172,5 +212,6 @@ api.add_resource(EmailApi, "/EmailApi")
 api.add_resource(CrawlerApi, "/CrawlerApi")
 api.add_resource(SurveyApi, "/SurveyApi")
 
+##run the flask
 def runFlask():
     app.run(debug=True)
