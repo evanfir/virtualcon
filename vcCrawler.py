9a6242ce1907349b24614861f030914eb9d9ec61
Evan Firoozi <evanfir@gmail.com>
2018-12-04 18:11:13 -0800

    added UCI, UCLA, UCD, and UCB
    Now the crawler can find relevant information on CS major from UCI, UCB, UCD, and UCLA

--- vcCrawler.py
+++ vcCrawler.py
@@ -4,21 +4,35 @@ from bs4 import BeautifulSoup
 class VirtualCrawler:
     def __init__(self, toCollege, fromCollege = "PASADENA"):
         self.__fromCollege = fromCollege
-        self.__toCollege = toCollege
+        if toCollege == "UCB": #UC Berkeley
+            self.__toCollege = "UCB"
+            self.__major = "CS-AB"
+        if toCollege == "UCD": #UC Davis
+            self.__toCollege = "UCD"
+            self.__major = "COMP.SCI.B.S."
+        if toCollege == "UCLA": #UC Los Angeles
+            self.__toCollege = "UCLA"
+            self.__major = "COMP+SCI"
+        if toCollege == "UCI": #UC Irvine
+            self.__toCollege = "UCI"
+            self.__major = "CS"
 
-    def getInfo(self):        
-        sourceCode = requests.get("http://web2.assist.org/web-assist/report.do?agreement=aa&reportPath=REPORT_2&reportScript=Rep2.pl&event=19&dir=1&sia=PASADENA&ria=UCB&ia=PASADENA&oia=UCB&aay=16-17&ay=16-17&dora=CS-AB")
+    ## getiFrameLink retrieve the iframe link from the assist.org page
+    # @return: link to the iFrame
+    def getiFrameLink(self):     
+        url = "http://web2.assist.org/web-assist/report.do?agreement=aa&reportPath=REPORT_2&reportScript=Rep2.pl&event=19&dir=1&sia=PASADENA&ria=" + self.__toCollege + "&ia=PASADENA&oia=" + self.__toCollege + "&aay=16-17&ay=16-17&dora=" + self.__major  
+        sourceCode = requests.get(url)
         plainText = sourceCode.text
         soup = BeautifulSoup(plainText, "html.parser")
         iframe = soup.find('iframe')
         iframeLink = iframe['src']
-        iframeSource = requests.get(iframeLink)
-        iframeText = iframeSource.text
-        return iframeText
+        # iframeSource = requests.get(iframeLink)
+        # iframeText = iframeSource.text
+        return iframeLink
 
 
 def main():
-    vc = VirtualCrawler("test1", "test2")
-    print(vc.getInfo())
+    vc = VirtualCrawler("UCLA")
+    print(vc.getiFrameLink())
 
-# main()
+main()
